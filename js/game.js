b194c80110718d802855fd27c35e968eba75fee0
DIEGO RODRIGUES <diego.sfc100@hotmail.com>
2021-03-03 11:03:53 -0300

    Fetch API Questions from Open Trivia API

--- js/game.js
+++ js/game.js
@@ -1,3 +1,4 @@
+
 const question = document.getElementById('question');
 const choices = Array.from(document.getElementsByClassName('choice-text'));
 const progressText = document.getElementById('progressText');
@@ -11,12 +12,31 @@ let availableQuesions = [];
 
 let questions = [];
 
-fetch('questions.json')
+fetch("https://opentdb.com/api.php?amount=50&category=11")
+
     .then((res) => {
         return res.json();
     })
     .then((loadedQuestions) => {
-        questions = loadedQuestions;
+        questions = loadedQuestions.results.map((loadedQuestion) => {
+            const formattedQuestion = {
+                question: loadedQuestion.question,
+            };
+
+            const answerChoices = [...loadedQuestion.incorrect_answers];
+            formattedQuestion.answer = Math.floor(Math.random() * 4) + 1;
+            answerChoices.splice(
+                formattedQuestion.answer - 1,
+                0,
+                loadedQuestion.correct_answer
+            );
+
+            answerChoices.forEach((choice, index) => {
+                formattedQuestion['choice' + (index + 1)] = choice;
+            });
+
+            return formattedQuestion;
+        });
         startGame();
     })
     .catch((err) => {
